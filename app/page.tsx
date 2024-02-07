af3d518952583d64c518aae0cb326104e6568e24
RobertoPaler <rpaler1113@gmail.com>
2024-02-07 13:13:36 +0800

    Installed Shadcn Components 2

--- app/page.tsx
+++ app/page.tsx
@@ -3,14 +3,5 @@ import { Calendar } from "@/components/ui/calendar";
 import { Progress } from "@/components/ui/progress";
 
 export default function Home() {
-  return (
-    <main className="container">
-      <Button variant={"default"}>asdasd</Button>
-      <Button className="bg-red-300" variant={"secondary"}>
-        asdasd
-      </Button>
-      <Calendar></Calendar>
-      <Progress></Progress>
-    </main>
-  );
+  return <main className="container"></main>;
 }
