522766a813dd927784995582dd19f7d08d614d78
joshuasemana <joshuadrummer14@gmail.com>
2024-02-07 16:51:47 +0800

    Fixed theme provider error

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,3 @@
-import { Button } from "@/components/ui/button";
-import { Calendar } from "@/components/ui/calendar";
-import { Progress } from "@/components/ui/progress";
-
 export default function Home() {
   return <main className="container"></main>;
 }
