58970c3064948bed11d22a1877db0a3728b893b1
RobertoPaler <rpaler1113@gmail.com>
2024-02-08 16:42:47 +0800

    Partial Login Register

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,20 @@
-import { Button } from "@/components/ui/button";
-import { Calendar } from "@/components/ui/calendar";
-import { Progress } from "@/components/ui/progress";
+import Login from "@/components/Login";
+import Registration from "@/components/Registration";
 
 export default function Home() {
-  return <main className="container"></main>;
+  return (
+    <main>
+      <div className="flex h-screen flex-col-reverse max-w-7xl mx-auto sm:flex-row">
+        <div className=" flex flex-1   justify-center items-center">
+          Carousel
+        </div>
+        <div className="flex flex-1 justify-center items-center">
+          <Login />
+          <div className="">
+            <Registration />
+          </div>
+        </div>
+      </div>
+    </main>
+  );
 }
